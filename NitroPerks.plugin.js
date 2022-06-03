eb41f44cad9125d6ff705a2327db3f028ce3d723
ramil12212 <94550861+ramil12212@users.noreply.github.com>
2022-06-03 23:57:09 +0500

    Update NitroPerks.plugin.js

--- NitroPerks.plugin.js
+++ NitroPerks.plugin.js
@@ -1,150 +1,91 @@
-/**
- * @name NitroPerks
- * @website https://github.com/respecting/NitroPerks
- * @source https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js
- * @updateUrl https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js
- */
-/*@cc_on
-@if (@_jscript)
-	
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+const Discord = require('discord.js') // подключение библиотеки                  Видео про бота https://youtu.be/1lzPIhTaPDY
+const client = new Discord.Client() // создание клиента
+
+client.on('ready', () =>{ // ивент, когда бот запускается https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-ready
+    client.generateInvite("ADMINISTRATOR").then(invite => console.log(`Ссылка на добавление ${invite}`))
+    console.log(`Привет! ${client.user.tag} запустился!`) // информация в консоль про успешный запуск
+})
+
+client.on('message', message =>{ // ивент, когда приходит любое сообщение в чат https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-message
+    if (message.author.bot) return; // если автор сообщения - бот, ничего не происходит 
+    if (message.content == '!профиль') { // если пользователь написал "!профиль" 
+    let embed = new Discord.MessageEmbed() // создание ембед сообщения
+    .setTitle(message.author.username) // в тайтле имя автора 
+    let status = ''
+    switch (message.author.presence.status) { // проверка статусов 
+        case 'online':
+            status = 'онлайн'; break; 
+            case 'idle':
+                status = ':orange_circle:нет на месте'; break;
+                case 'offline':
+                    status = 'нет в сети'; break;
+                    case 'dnd':
+                        status = ':red_circle:не беспокоить'; break;
     }
-    WScript.Quit();
+    embed.setDescription(`**Ваш дискорд айди: ${message.author.id}
+    Ваш статус: ${status}
+    Дата создания аккаунта: ${message.author.createdAt.toLocaleDateString()}
+    Дата входа на сервер: ${message.member.joinedAt.toLocaleDateString()}
+    **`) // описание ембеда
+    .setColor('RANDOM') // рандомный цвет ембеда
+    .setThumbnail(message.author.avatarURL()) // вставляем в ембед аватарку пользователя
+    message.channel.send(embed) // отправляем сообщение в канал где была написана команда   
+    }
+})
 
-@else@*/
-module.exports = (() => {
-    const config = {
-        "info": {
-            "name": "NitroPerks",
-            "authors": [{
-                "name": "lemons",
-                "discord_id": "407348579376693260",
-                "github_username": "respecting"
-            }],
-            "version": "2.0.0",
-            "description": "Abandoned!",
-            "github": "https://github.com/respecting/NitroPerks",
-            "github_raw": "https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js"
-        },
-        "main": "NitroPerks.plugin.js"
-    };
+client.on('messageDelete', message =>{ // ивент, когда удаляется любое сообщение с сервера https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberAdd
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Было удалено сообщение!')
+    .setColor('RANDOM')
+    .addField(`Удалённое сообщение:`, message.content, true)
+    .addField("Автор:",`${message.author.tag} (${message.author})`,true)
+    .addField("Канал:", `${message.channel}`, false)
+    .setFooter(' - ',`${message.author.avatarURL()}`)
+    .setTimestamp(message.createdAt);
+  client.channels.cache.get("АЙДИ КАНАЛА С ЛОГАМИ").send(embed); // айди вашего канала с логами
+})
 
-    return !global.ZeresPluginLibrary ? class {
-        constructor() {
-            this._config = config;
-        }
-        getName() {
-            return config.info.name;
-        }
-        getAuthor() {
-            return config.info.authors.map(a => a.name).join(", ");
-        }
-        getDescription() {
-            return config.info.description;
-        }
-        getVersion() {
-            return config.info.version;
-        }
-        load() {
-            BdApi.showConfirmationModal("Notice:",
-                [
-                    "NitroPerks has been abandoned.",
-                    "Alternatives you can use are:",
-                    BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
-                    BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
-                ],
-                {
-                    danger: true,
-                    confirmText: "Okay.",
-                    cancelText: ""
-                }
-            );
-        }
-        start() { }
-        stop() { }
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Api) => {
-            return class NitroPerks extends Plugin {
-                getSettingsPanel() {
-                    return BdApi.showConfirmationModal("Notice:",
-                        [
-                            "NitroPerks has been abandoned.",
-                            "Alternatives you can use are:",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
-                        ],
-                        {
-                            danger: true,
-                            confirmText: "Okay.",
-                            cancelText: ""
-                        }
-                    );
-                }
+client.on('guildMemberAdd', member =>{ // ивент, когда пользователь присоединяется к серверу https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberAdd
+    let embed = new Discord.MessageEmbed()
+    .setThumbnail(member.user.avatarURL())
+    .setTitle(`Привет, ${member.user.username}!`)
+    .setDescription(`**Ты попал на мой сервер!
+    Ты наш \`${client.guilds.get("АЙДИ СЕРВЕРА").memberCount}\` участник! **`) // айди вашего сервера               !!!!!!!!!!
+    .setFooter('Будь всегда на позитиве :3', 'https://cdn.discordapp.com/emojis/590614597610766336.gif?v=1')
+    // .addField(`Участвуй в розыгрышах!`, `<#706487236220289054>`, true) // Добавляйте свои каналы по желанию
+    // .addField(`Общайся в чате!`, `<#702364684199788625>`, true)
+    // .addField(`Смотри видео наших ютуберов!`, `<#702363551184060546>`, true)
+    .setColor('RANDOM')
+    member.send(embed); // отправка сообщения в лс 
 
-                saveAndUpdate() {
-                    BdApi.showConfirmationModal("Notice:",
-                        [
-                            "NitroPerks has been abandoned.",
-                            "Alternatives you can use are:",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
-                        ],
-                        {
-                            danger: true,
-                            confirmText: "Okay.",
-                            cancelText: ""
-                        }
-                    );
-                }
-                onStart() {
-                    BdApi.showConfirmationModal("Notice:",
-                        [
-                            "NitroPerks has been abandoned.",
-                            "Alternatives you can use are:",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
-                        ],
-                        {
-                            danger: true,
-                            confirmText: "Okay.",
-                            cancelText: ""
-                        }
-                    );
-                }
+    let embed2 = new Discord.MessageEmbed()
+    .setThumbnail(member.user.avatarURL())
+    .setTitle(`Пользователь вошел на сервер`)
+    .addField('Пользователь:', member.user)
+    .setColor('RANDOM')
+    member.send(embed);
+    client.channels.cache.get('АЙДИ КАНАЛА С ЛОГАМИ').send(embed2) // айди вашего канала с логами
+})
 
-                onStop() {
-                    BdApi.showConfirmationModal("Notice:",
-                        [
-                            "NitroPerks has been abandoned.",
-                            "Alternatives you can use are:",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
-                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
-                        ],
-                        {
-                            danger: true,
-                            confirmText: "Okay.",
-                            cancelText: ""
-                        }
-                    );
-                }
-            };
-        };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
-/*@end@*/
+client.on('guildMemberRemove', member => { // ивент, когда пользователь выходит с сервера https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildMemberRemove
+    let embed = new Discord.MessageEmbed()
+    .setThumbnail(member.user.avatarURL())
+    .setTitle(`Пользователь покинул сервер`)
+    .addField('Пользователь:', member.user)
+    .setColor('RANDOM')
+    member.send(embed);
+    client.channels.cache.get('АЙДИ КАНАЛА С ЛОГАМИ').send(embed) // айди вашего канала с логами
+  })
+
+async function change() {
+    let members = client.guilds.cache.get("АЙДИ ВОЙСА").memberCount // сколько людей на сервере + указать айди своего сервера
+    client.channels.cache.get("АЙДИ СЕРВЕРА").setName(`На сервере: ${members}`); // свой айди войса
+}
+
+var interval = setInterval(function () { change(); }, 20000  ); // время обновления в миллисекундах
+
+client.login('OTgyMjEyMzc0MDkyMTk3OTUw.G48tIc.u_7qLFwQk-Kn6DiIIhEpmLunjpbikb2OGDp1Ec') // токен вашего бота
+
+// Хотите, чтобы ваш бот работал 24/7 бесплатно? Смотрите это видео: https://www.youtube.com/watch?v=wxdl4QK0am4
+
+// Bot by Sanich https://youtube.com/sanich - фишки, гайды по приложению Discord
